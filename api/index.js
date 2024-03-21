30cac0cef4f9094febeccaae95bdf9aba42ca91c
OliverRusta <OliverRusta34@gmail.com>
2024-03-21 20:12:09 +0800

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+
+mongoose
+    .connect(
+        process.env.MONGO
+        )
+    .then(() => { 
+        console.log("mongodb is connected");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
