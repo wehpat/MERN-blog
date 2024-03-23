85dd0b2cd829c414cad04c6a764acf21fe041710
OliverRusta <OliverRusta34@gmail.com>
2024-03-24 06:53:56 +0800

    create signup API route

--- api/index.js
+++ api/index.js
@@ -2,18 +2,17 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
+import authRoute from "./routes/auth.route.js"
 
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-app.use('/api/user', userRoute)
+app.use(express.json());
 
 mongoose
-    .connect(
-        process.env.MONGO
-        )
+    .connect(process.env.MONGO)
     .then(() => { 
         console.log("mongodb is connected");
         app.listen(PORT, () => {
@@ -23,3 +22,16 @@ mongoose
     .catch((error) => {
         console.log(error);
     });
+
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
--- api/models/user.mode.js
+++ api/models/user.mode.js
@@ -23,3 +23,5 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+export default User;
+
