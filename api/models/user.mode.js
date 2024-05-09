38c9e2d03e3ab5c33c2642d2dbda1768b1258244
OliverRusta <OliverRusta34@gmail.com>
2024-05-09 09:07:27 +0800

    add admin functionality

--- api/models/user.mode.js
+++ api/models/user.mode.js
@@ -20,7 +20,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         default:
             'https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg'
-    }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 
 }, {timestamps: true}
 
