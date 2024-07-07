dfd8df0158afe9067407c631f54fa8ebeeb79a14
OliverRusta <OliverRusta34@gmail.com>
2024-07-07 20:51:25 +0800

    complete about and porjects pages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function FooterCom() {
                 >
                     <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
                      via-purple-500 to-pink-500 rounded-lg text-white'>
-                        Chan's 
+                        Jan's 
                     </span>
                     Blog
                 </Link>
@@ -37,7 +37,7 @@ export default function FooterCom() {
                             target='_blank'
                             rel='noopener noreferrer'
                             >
-                                Chan's Blog
+                                Jan's Blog
                         </Footer.Link>
                     </Footer.LinkGroup>
                     </div>
@@ -83,7 +83,7 @@ export default function FooterCom() {
             <div className='w-full sm:flex sm:items-center sm:justify-between'>
                 <Footer.Copyright
                     href="#"
-                    by="Chan's blog"
+                    by="Jan's blog"
                     year={new Date().getFullYear()}
                 />
                 <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
