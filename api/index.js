ed32219e41cfdcb8904a75250ea358763d8a41ac
OliverRusta <OliverRusta34@gmail.com>
2024-07-08 08:26:52 +0800

    rename files

--- api/index.js
+++ api/index.js
@@ -36,11 +36,11 @@ app.use('/api/auth', authRoute);
 app.use('/api/post', postRoute);
 app.use('/api/comment', commentRoute);
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
